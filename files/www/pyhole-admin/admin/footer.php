becb66ef549e9b392462ae9c78443815ca47b8dd
René <tylonhh+github@gmail.com>
2016-09-19 22:27:08 +0200

    Update footer.php
    Open external Links in new pages

--- files/www/pyhole-admin/admin/footer.php
+++ files/www/pyhole-admin/admin/footer.php
@@ -7,7 +7,7 @@
             <b>Pi-hole Version </b> <?php echo exec("cd /etc/.pihole/ && git describe --tags --abbrev=0"); ?>
             <b>Web Interface Version </b> <?php echo exec("cd /var/www/html/admin/ && git describe --tags --abbrev=0"); ?>
         </div>
-        <i class="fa fa-github"></i> <strong><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=3J2L3Z4DHW9UY">Donate</a></strong> if you found this useful.
+        <i class="fa fa-github"></i> <strong><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=3J2L3Z4DHW9UY" target="_blank">Donate</a></strong> if you found this useful.
     </footer>
 </div>
 <!-- ./wrapper -->
